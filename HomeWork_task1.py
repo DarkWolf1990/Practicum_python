91e387ba7fb1544d77aaab54851097610ef39405
DarkWolf_ <whitejaguar08@gmail.com>
2022-07-10 13:38:31 +0300

    completed task1

--- HomeWork_task1.py
+++ HomeWork_task1.py
@@ -1,9 +1,31 @@
 # Напишите программу, которая принимает на вход цифру, 
 # обозначающую день недели, и проверяет, 
 # является ли этот день выходным.
-#     Пример:
+# Пример:
 # - 6 -> да
 # - 7 -> да
 # - 1 -> нет
 dayWeek = input(" введите число дня недели: ")
+monday = "1"
+tuesday = "2"
+wednesday = "3"
+thusday = "4"
+friday = "5"
+saturday = "6"
+sunday = "7"
+if dayWeek == monday:
+  print("monday")
+elif dayWeek ==tuesday:
+  print("tuesday")
+elif dayWeek == wednesday:
+  print("wednesday")
+elif dayWeek == thusday:
+  print("thusday")
+elif dayWeek == friday:
+  print("friday")
+elif dayWeek == saturday:
+  print("saturday")
+else:
+  dayWeek == sunday
+  print("sunday")
 
