30670a83f3a499f13f9c35c90c4b53b9797f9443
DarkWolf_ <whitejaguar08@gmail.com>
2022-07-10 17:57:04 +0300

    update-task1

--- HomeWork_task1.py
+++ HomeWork_task1.py
@@ -7,29 +7,38 @@
 # - 1 -> нет
 
 num = int(input(" введите число дня недели: "))
-def dayWeek(num):
-  monday = 1
-  tuesday = 2
-  wednesday = 3
-  thusday = 4
-  friday = 5
-  saturday = 6
-  sunday = 7
-  if num == monday:
-    print("Нет")
-  elif num ==tuesday:
-    print("нет")
-  elif num == wednesday:
-    print("нет")
-  elif num == thusday:
-    print("нет")
-  elif num == friday:
-    print("нет")
-  elif num == saturday:
-    print("Да")
-  else:
-    num == sunday
-    print("Да")
+# def dayWeek(num):
+#   monday = 1
+#   tuesday = 2
+#   wednesday = 3
+#   thusday = 4
+#   friday = 5
+#   saturday = 6
+#   sunday = 7
+#   if num == monday:
+#     print("Нет")
+#   elif num ==tuesday:
+#     print("нет")
+#   elif num == wednesday:
+#     print("нет")
+#   elif num == thusday:
+#     print("нет")
+#   elif num == friday:
+#     print("нет")
+#   elif num == saturday:
+#     print("Да")
+#   else:
+#     num == sunday
+#     print("Да")
     
-dayWeek(num)
+# dayWeek(num)
 
+def dayWeek(num): 
+    if  num in range(1,5): 
+        print('Нет') 
+    elif num in range(6,7): 
+        print('Да') 
+    else: 
+        print('Такого дня недели нет, попробуйте еще раз!') 
+ 
+dayWeek(num)
