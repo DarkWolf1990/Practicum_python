dc87a51f0ee082791b38b5c658f131f46ab5f7b6
DarkWolf_ <whitejaguar08@gmail.com>
2022-07-29 20:57:59 +0300

    update

---
+++ lesson_7/logger.py
@@ -0,0 +1,25 @@
+from datetime import datetime as dt
+from time import time 
+
+def _______________logger(data):
+  time = dt.now().strftime('%H:%M')
+  with open('log.txt', 'a') as file:
+    file.write('{}; __________________;{}\n'
+                .format(time, data))
+    
+    
+def ___________logger(data):
+  time = dt.now().strftime('%H:%M')
+  with open('log.txt', 'a') as file:
+    file.write('{}; ________;{}\n'
+                .format(time, data))
+        
+        
+def ______________logger(data):
+  time = dt.now().strftime('%H:%M')
+  with open('log.txt', 'a') as file:
+    file.write('{}; ****;{}\n'
+                .format(time, data))
+    
+    
+    esase gui
